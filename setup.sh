98ec883608fd6509b423d3045a776f5c34869a0f
qiujialong <barryqiu@wedochina.cn>
2020-08-20 11:16:27 +0800

    latest commit

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 image_version='date +%Y%m%d%H%M';
 echo $image_version;
-# cd docker
+# cd Jenkins2Docker
 git pull --rebase origin master;
-sudo docker stop docker;
-sudo docker rm docker;
-sudo docker build -t docker:$image_version .;
+sudo docker stop jenkins2docker;
+sudo docker rm jenkins2docker;
+sudo docker build -t jenkins2docker:$image_version .;
 sudo docker images;
-sudo docker run -p 10001:80 -d --name docker docker:$image_version;
+sudo docker run -p 10001:80 -d --name jenkins2docker jenkins2docker:$image_version;
 # -v ~/docker-data/house-web/appsettings.json:/app/appsettings.json -v ~/docker-data/house-web/NLogFile/:/app/NLogFile   --restart=always
-sudo docker logs docker;
+sudo docker logs jenkins2docker;
 #删除build过程中产生的镜像    #docker image prune -a -f
 sudo docker rmi $(docker images -f "dangling=true" -q)
