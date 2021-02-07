39e2bfe9ab9526715225678ac44f51969add3662
qiujialong <qiujialong_jsj@sina.com>
2021-02-07 18:47:06 +0800

    first commit3

--- setup.sh
+++ setup.sh
@@ -1,11 +1,9 @@
-image_version='date+%Y%m%d%H%M';
-echo $image_version;
 git pull --rebase origin master;
 sudo docker stop jenkins2docker;
 sudo docker rm jenkins2docker;
-sudo docker build -t jenkins2docker:$image_version .;
+sudo docker build -t jenkins2docker .;
 sudo docker images;
-sudo docker run -p 54907:80 -d --name jenkins2docker jenkins2docker:$image_version;
+sudo docker run -p 54907:80 -d --name jenkins2docker jenkins2docker;
 # -v ~/docker-data/house-web/appsettings.json:/app/appsettings.json -v ~/docker-data/house-web/NLogFile/:/app/NLogFile   --restart=always
 sudo docker logs jenkins2docker;
 #删除build过程中产生的镜像    #docker image prune -a -f
