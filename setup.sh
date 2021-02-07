78f0d624a9a5822dbdac84d0d96d2f8dd233ad5d
Jess Smith <shisujie@outlook.com>
2021-02-07 17:45:34 +0800

    first commit

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-image_version='date +%Y%m%d%H%M';
+image_version='date+%Y%m%d%H%M';
 echo $image_version;
 # cd Jenkins2Docker
 git pull --rebase origin master;
@@ -10,4 +10,4 @@ sudo docker run -p 54907:80 -d --name jenkins2docker jenkins2docker:$image_versi
 # -v ~/docker-data/house-web/appsettings.json:/app/appsettings.json -v ~/docker-data/house-web/NLogFile/:/app/NLogFile   --restart=always
 sudo docker logs jenkins2docker;
 #删除build过程中产生的镜像    #docker image prune -a -f
-sudo docker rmi $(docker images -f "dangling=true" -q)
+sudo docker rmi $(docker images -f "dangling=true" -q);
